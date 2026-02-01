c8c45dde5b1bbc83edd9745e27cd1c5756a2e385
HauHa-Healthfox <hau.ha@healthfox.fi>
2026-02-01 05:34:13 +0200

    feat: implement user sign-in functionality with JWT and session management

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,5 +1,11 @@
 import bcrypt from "bcrypt";
 import User from "../models/User.js";
+import jwt from "jsonwebtoken";
+import crypto from "crypto";
+import Session from "../models/Session.js";
+
+const ACCESS_TOKEN_TTL = "30m"; // 15 minutes
+const REFRESH_TOKEN_TTL = 14 * 24 * 60 * 60 * 1000; // 14 days in seconds
 
 export const signUp = async (req, res) => {
   try {
@@ -33,6 +39,61 @@ export const signUp = async (req, res) => {
     return res.status(204).json({ message: "User created successfully" });
   } catch (error) {
     console.error("Error during sign up:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const signIn = async (req, res) => {
+  try {
+    // take inputs
+    const { username, password } = req.body;
+    if (!username || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+    // take hashed password from db to compare with password input
+    const user = await User.findOne({ username });
+
+    if (!user) {
+      return res
+        .status(401)
+        .json({ message: "username or passrord not correct" });
+    }
+
+    //check password with bcrypt
+    const isMatch = await bcrypt.compare(password, user.hashedPassword);
+    if (!isMatch) {
+      return res
+        .status(401)
+        .json({ message: "username or passrord not correct" });
+    }
+
+    //if correct , create access token with jwt
+    const accessToken = jwt.sign(
+      { userId: user._id },
+      process.env.ACCESS_TOKEN_SECRET_KEY,
+      { expiresIn: ACCESS_TOKEN_TTL }
+    );
+
+    // create refresh token with jwt
+    const refreshToken = crypto.randomBytes(64).toString("hex");
+
+    //create new session to store refresh token
+    await Session.create({
+      userId: user._id,
+      refreshToken,
+      expiresAt: new Date(Date.now() + REFRESH_TOKEN_TTL),
+    });
+    //return refesh token in cookie
+    res.cookie("refreshToken", refreshToken, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+      maxAge: REFRESH_TOKEN_TTL,
+    });
+    //return access token in response
+    return res.status(200).json({ message: "Sign in successful", accessToken });
+  } catch (error) {
+    console.error("Error during sign in:", error);
     res.status(500).json({ message: "Internal server error" });
   }
 };
--- backend/src/routes/authRoute.js
+++ backend/src/routes/authRoute.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { signUp } from "../controllers/authController.js";
+import { signUp, signIn } from "../controllers/authController.js";
 
 const router = express.Router();
 
 // Define auth routes here
 router.post("/signup", signUp);
 
+router.post("/signin", signIn);
 export default router;
